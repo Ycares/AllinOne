40d4395c24416bfa011fdb897eec873168ab7751
Elanzize ALI OUSSENI <elift225@gmail.com>
2023-12-27 10:58:07 +0100

    c v.2

--- src/Controller/CompetenceController.php
+++ src/Controller/CompetenceController.php
@@ -15,43 +15,23 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/competences', name: 'competence_')]
 class CompetenceController extends AbstractController
 {
-    // #[Route('/listcom', name: 'one_competence')]
-    // public function list(CompetencesRepository $competencesRepository): Response
-    // {
-    //     $competences = $competencesRepository->findAll();
-
-    //     return $this->render('competence/competence.html.twig', [
-    //         'competences' => $competences,
-    //     ]);
-    // }
-  
     #[Route('/listcom/{competences}', name: 'one_competence')]
     public function onelist(Competences $competences): Response
     {
-   
+
         return $this->render('competence/competence.html.twig', [
             'competences' => $competences,
-            
+
         ]);
     }
 
-    // #[Route('/listcat', name: 'list_categorie')]
-    // public function index(CategoriesCompetenceRepository $categoriesRepository): Response
-    // {
-    //     $categories = $categoriesRepository->findAll();
-
-
-    //     return $this->render('competence/categorie.html.twig', [
-    //         'categories' => $categories,
-    //     ]);
-    // }
     #[Route('/listcat', name: 'list_categorie')]
     public function index(CompetencesRepository $competencesRepository): Response
     {
         $competences = $competencesRepository->findAll();
 
 
-        return $this->render('competence/cat.html.twig', [
+        return $this->render('competence/categorie.html.twig', [
             'competences' => $competences,
         ]);
     }
@@ -64,33 +44,30 @@ class CompetenceController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            
+
 
             $em->persist($competence);
             $em->flush();
         }
-            return $this->render('competence/ajout.html.twig', [
-                
-                'competenceForm' => $form
-            ]);
+        return $this->render('competence/ajout.html.twig', [
+
+            'competenceForm' => $form
+        ]);
     }
-    
+
     #[Route('/edit/{competence}', name: 'edit')]
-    public function parametre(Competences $competence,Request $request): Response
+    public function parametre(Competences $competence, Request $request): Response
     {
-        
+
         $form = $this->createForm(CompetenceType::class, $competence);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            
-
-           
         }
-            return $this->render('competence/parametre.html.twig', [
-                
-                'competenceForm' => $form
-            ]);
+        return $this->render('competence/parametre.html.twig', [
+
+            'competenceForm' => $form
+        ]);
     }
 }
